c75094ff28625166cf5ef4501d61e3cc39dc41cf
matusala3 <matusalakiflom@gmail.com>
2025-02-28 20:43:21 +0200

    Add appointments, contact, and UI components for enhanced functionality

--- src/app/ui/Navbar.js
+++ src/app/ui/Navbar.js
@@ -10,19 +10,25 @@ const Navbar = () => {
       </h1>
       <div className="hidden md:flex space-x-6">
         <Link
+          href="/"
+          className="border-b-2 border-transparent hover:border-blue-400 transition-colors"
+        >
+          Home
+        </Link>
+        <Link
           href="/carlisting"
           className="border-b-2 border-transparent hover:border-blue-400 transition-colors"
         >
           Car Listings
         </Link>
         <Link
-          href="/"
+          href="/appointments"
           className="border-b-2 border-transparent hover:border-blue-400 transition-colors"
         >
           Appointments
         </Link>
         <Link
-          href="/"
+          href="/contact"
           className="border-b-2 border-transparent hover:border-blue-400 transition-colors"
         >
           Contact Us
