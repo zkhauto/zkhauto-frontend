1be0973c9fcadec3b0e6578f20b13c89678ec142
Mst-Airen-Akter <aktarmstairen@gmail.com>
2025-05-15 23:35:41 +0600

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 
-import { useRouter } from "next/router";
 import { AuthProvider } from "./context/AuthContext";
 import "./globals.css";
 import Footer from "./ui/Footer";
@@ -13,8 +12,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const router = useRouter();
-  console.log("Current location:", router);
   return (
     <html lang="en">
       <body className={inter.className}>
